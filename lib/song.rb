3ef61ce9a38e6bae5e90b56fc6038866f7a8aa9f
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-17 07:39:44 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,19 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song
   @@all = []
+
+  def self.create
+    # @song = Song.new
+    @song = Song.new
+    @song.save
+    @song
+  end
+
+  def self.destroy_all
+    @@all.clear
+
+  end
+
 
   def self.all
     @@all
@@ -9,5 +22,55 @@ class Song
   def save
     self.class.all << self
   end
+
+  # def self.create
+  #   @@all << Song.new
+  # end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    song = @@all.find do |song|
+      song.name == name
+    end
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.split(".")
+    songAndArtist = filename[0].split(" - ")
+    song=Song.new
+
+    song.name = songAndArtist[1]
+    song.artist_name = songAndArtist[0]
+
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = Song.new_from_filename(filename)
+    song.save
+    song
+  end
+
 
 end
